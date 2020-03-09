826c25ecd44f1dcb9b47b5970a973076c11b910d
jchiter <j.chiter@yandex.ru>
2020-03-10 01:21:30 +0300

    Адаптивность для телефона

--- pages/shared/main.php
+++ pages/shared/main.php
@@ -8,8 +8,6 @@
             <div class="grid-item" id="new-order"><i class="fa fa-address-card-o"></i><a class="link" href="?p=new_order"></a>новый заказ</div>
             <div class="grid-item" id="all-order"><i class="fa fa-list-alt"></i><a class="link" href="?p=orders"></a>список заказов</div>
             <div class="grid-item" id="history"><i class="fa fa-history"></i><a class="link" href="?p=history"></a>история</div>
-        </div>
-        <div class="dashboard-grid">
             <div class="grid-item" id="profile"><i class="fa fa-user-o"></i><a class="link" href="?p=profile"></a>профиль</div>
             <div class="grid-item" id="exit" onclick="$.coremanage.systemExit()"><i class="fa fa-sign-out"></i>выход</div>
         </div>
